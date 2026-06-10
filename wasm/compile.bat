09a7056b4e19b35b0ac51b18d8f7b8c78b240282
Mehul raj <your_registered_email@gmail.com>
2026-06-10 22:04:39 +0530

    fix: export HEAPU8 in WASM build, commit compiled artifacts

--- wasm/compile.bat
+++ wasm/compile.bat
@@ -5,7 +5,7 @@ emcc wasm/box_counting.cpp ^
   -s MODULARIZE=1 ^
   -s EXPORT_NAME="createBoxCountingModule" ^
   -s EXPORTED_FUNCTIONS="['_wasm_run_analysis','_wasm_free','_malloc','_free']" ^
-  -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap','UTF8ToString','lengthBytesUTF8','stringToUTF8','allocate','ALLOC_NORMAL']" ^
+  -s EXPORTED_RUNTIME_METHODS="['UTF8ToString','HEAPU8']" ^
   -s ALLOW_MEMORY_GROWTH=1 ^
   -s ENVIRONMENT="web" ^
   -s SINGLE_FILE=0 ^
